e98ca840f5ced6bca96e9ad315de147fcbb742d4
andreafrittella <andreafrittella@macmini.fritz.box>
2024-09-27 15:47:16 +0200

    Helloooooo metodo 1

--- src/main/java/it/generationitaly/app/controller/App.java
+++ src/main/java/it/generationitaly/app/controller/App.java
@@ -1,7 +1,12 @@
 package it.generationitaly.app.controller;
 
 public class App {
-  public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+	  public static void main(String[] args) {
+		System.out.println("Hello World");
+		metodo1();
+	}
+	
+	private static void metodo1() {
+		System.out.println("Hello metodo1");
+	}
 }
